24a5913abfe939bff686e10bc150d9234695b64a
HiramZednem <hirammendez000@gmail.com>
2023-03-28 22:25:54 -0600

    feature/AMAZON-REMASTERED-7 update

---
+++ src/test/java/com/escuelita/demo/services/impl/UserServiceImplTest.java
@@ -0,0 +1,9 @@
+package com.escuelita.demo.services.impl;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplTest {
+
+}
