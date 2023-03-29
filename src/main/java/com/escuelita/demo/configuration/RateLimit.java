554700bdd706b8fbdc43a06ffc86bd2630c342fc
HiramZednem <hirammendez000@gmail.com>
2023-03-29 09:29:13 -0600

    features/AMAZON-REMASTERED-21 refactor configuration

--- src/main/java/com/escuelita/demo/configuration/RateLimit.java
+++ src/main/java/com/escuelita/demo/configuration/RateLimit.java
@@ -1,12 +1,10 @@
 package com.escuelita.demo.configuration;
 
 import java.io.IOException;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
